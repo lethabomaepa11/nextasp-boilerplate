206ffa71755b52b63f1e2a63974df99fd0356503
Lethabo Maepa <lethabomaepa11@gmail.com>
2026-05-24 20:22:30 +0200

    Added the CORS for nextjs frontend on port 3000

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -6,6 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("web", p =>
+        p.WithOrigins("http://localhost:3000")
+         .AllowAnyHeader()
+         .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +26,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
+app.UseCors("web");
 
 app.MapControllers();
 
